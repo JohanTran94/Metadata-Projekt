1a457e7ab9c7786716fc0b8b6f6857ec8458b466
thomasavguld <rosen.thomas@gmail.com>
2025-08-21 10:52:19 +0200

    added file name to objects

--- test_route.js
+++ test_route.js
@@ -7,17 +7,33 @@ let json = fs.readFileSync('./powerpoint.json', 'utf-8');
 // Convert from a string to a real data structure
 let data = JSON.parse(json);
 
-for (let powerpointMetadata of data.slice(0, 25)) {
+for (let powerpointMetadata of data.slice(0, 5)) {
+
   // extract the file name (the property digest + '.ppt)
-  let fileName = powerpointMetadata.digest + '.ppt';
+  let fileName = powerpointMetadata.digest + '.ppt'
 
+  // add file name to attributes
+  powerpointMetadata.file_name = fileName
 
-  // remove the unnecessary attributes
+  // remove unnecessary attributes
   delete powerpointMetadata.digest;
   delete powerpointMetadata.sha256;
   delete powerpointMetadata.sha512;
   delete powerpointMetadata.urlkey;
   delete powerpointMetadata.timestamp;
+
+  // clean mimetype 
+  let cleanedMimeType = (powerpointMetadata.mimetype || '')
+    .replace(/^(application\/+)/, '');
+
+
+  let newJson = JSON.stringify(powerpointMetadata, null, '  ');
+
+  console.log(newJson)
+
+
+
+
 
   // console.log things to see that we have correct 
   // filname and metadata
@@ -26,41 +42,40 @@ for (let powerpointMetadata of data.slice(0, 25)) {
   //console.log(fileName);
   //console.log(powerpointMetadata);
 
-  // Check for different mime types
-  // Create set of mime types in the JSON data
-  let mimeTypes = new Set();
-  for (let item of data) {
-    if (item.mimetype) {
-      mimeTypes.add(item.mimetype)
-    }
-  }
-
-  //Identify non-standard data types for removal
-
-  let toRemove = ['powerpoint', 'PowerPoint%202007%20File']
-
-  let cleanedMimeTypes = [...mimeTypes]
-
-    //Map the set
-    .map(type => type
-      //remove instances of the string "/application"
-      .replace(/^(application\/)+/, ''))
-    //filter out non-standard data types
-    .filter(type => !toRemove.includes(type));
-
-  cleanedMimeTypes = [...new Set(cleanedMimeTypes)];
-
-  console.log(data)
-
 
 }
 
-
-
-
+/*
 // TODO: Do something like this to INSERT the data in our database
 /*let result = await query(`
   INSERT INTO powerpoints (fileName, metadata)
   VALUES(?, ?)
 `, [fileName, powerPointMetadata]);
-console.log(result);*/
+console.log(result);
+
+
+//Identify non-standard data types for removal
+let toRemove = ['powerpoint', 'PowerPoint%202007%20File']
+
+// Check for different mime types
+// Create set of mime types found in the JSON data
+let mimeTypes = new Set();
+for (let item of data) {
+  if (item.mimetype) {
+    mimeTypes.add(item.mimetype)
+  }
+
+  let cleanedMimeTypes = [...mimeTypes]
+
+    //Map the set
+    
+
+  cleanedMimeTypes = [...new Set(cleanedMimeTypes)];
+
+
+
+.map(powerpointMetadata => powerpointMetadata
+      //remove instances of the string "/application"
+      .replace(/^(application\/)+/, ''))
+    //filter out non-standard data types
+    .filter(powerpointMetadata => !toRemove.includes(powerpointMetadata));*/
