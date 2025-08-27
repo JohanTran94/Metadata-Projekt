dbeca2770565f87b3adeec957285eae34e495d71
thomasavguld <rosen.thomas@gmail.com>
2025-08-27 14:55:13 +0200

    simplified db_imports (and changed file name to db_imports.js)

---
+++ db_import.js
@@ -0,0 +1,60 @@
+import fs from 'fs';
+import mysql from 'mysql2/promise';
+import dbCreds from './db.js';
+
+let cleanJson = fs.readFileSync('./cleanedPowerpointJson.json', 'utf-8');
+let data = JSON.parse(cleanJson);
+
+const db = await mysql.createConnection(dbCreds);
+
+await db.execute('DELETE FROM powerpoint_metadata');
+
+for (let file of data) {
+  let dbImport = await db.execute(`
+        INSERT INTO powerpoint_metadata (metadata)
+        VALUES (?)
+      `,
+    [JSON.stringify(file)]);
+  console.log(file, dbImport);
+
+}
+
+console.log('Metadata imported.')
+process.exit()
+
+
+
+
+
+
+
+
+
+
+/*
+
+console.log("Connected to the database!");
+
+// A small function for running queries
+async function query(sql, listOfValues) {
+  let result = await db.execute(sql, listOfValues);
+  return result[0];
+}
+
+// Insert data from JSON into database
+for (let file_name of data) {
+  console.log('A PowerPoint:');
+  console.log(file_name);
+
+  let result = await query(`
+        INSERT INTO powerpoint_metadata (metadata)
+        VALUES (?)
+      `,
+    [JSON.stringify(file_name)]);  // Store the entire metadata as a JSON string
+  console.log(result);
+}
+
+// Close the connection after the operations
+await db.end();
+
+*/
