17341e8d91f84d8576e226918f4a6419a06c2103
JonasLarsson-coder <Jonas.larsson.data24sto@edu.tucsweden.se>
2025-09-01 11:06:10 +0200

    update

--- frontend/main.js
+++ frontend/main.js
@@ -1,34 +1,61 @@
-// Grab the input field firstName
-let inputField = document.querySelector('input[name="firstName"]');
-// Listen to when the user types a character in the field
-inputField.addEventListener('keyup', async () => {
-  // Read the value of the input field
-  let searchValue = inputField.value;
-  // Grab the main element
-  let main = document.querySelector('main');
-  // If the value is empty do not try to search
-  // but empty the main element
-  if (searchValue === '') {
-    main.innerHTML = '';
-    return;
-  }
-  // Ask the database to search for users via a REST-api route
-  let rawData = await fetch('/api/search-by-firstname/' + searchValue);
-  // Convert rawData from json to a js data structure
-  let data = await rawData.json();
+// Read data from the json file metadata
+let metadataRaw = await fetch('/api/metadata');
+// Convert from json to a js data structure
+let metadata = await metadataRaw.json();
 
-  // Convert the data to html
-  let html = '';
-  for (let { id, firstName, lastName, email } of data) {
-    html += `
-      <article>
-        <h2>${firstName} ${lastName}</h2>
-        <p><b>Email:</b> ${email}</p>
-        <p><b>Id:</b> ${id}</p>
-      </article>
-    `;
+// Path to pdf folder
+let pdfFolder = 'all-pdfs';
+
+// loop through every pdf in the metadata
+for (let { file, numpages, info, xmp, text } of metadata) {
+
+  // create a info table for each pdf
+  let htmlTable = '<table>';
+
+  htmlTable += `<tr>
+    <td colspan="2">
+      <a target="_blank" href="${pdfFolder}/${file}">Link to PDF</a>
+    </td>
+  </tr>`;
+  htmlTable += `<tr>
+    <td>Number of pages:</td>
+    <td>${numpages}</td>
+  </tr>`;
+  htmlTable += `
+    <tr><td colspan="2">
+      <b>Text extract:</b><br>
+      ${text || 'No text (maybe a scanned document)'}
+    </td></tr>
+  `;
+  htmlTable += `<tr><td colspan="2">
+    <b>Info (old type of metadata)</b>
+  </td></tr>`;
+  for (let key in info) {
+    let value = info[key];
+    htmlTable += `<tr>
+      <td>${key}</td>
+      <td>${value}</td>
+    </tr>`;
   }
-  // Replace the content of the main element with
-  // our new html (the data converted from json)
-  main.innerHTML = html;
-});
+  if (Object.keys(xmp).length !== 0) {
+    htmlTable += `<tr><td colspan="2">
+    <b>XMP (new type of metadata)</b>
+  </td></tr>`;
+    for (let key in xmp) {
+      let value = xmp[key];
+      htmlTable += `<tr>
+      <td>${key}</td>
+      <td>${value}</td>
+    </tr>`;
+    }
+  }
+  htmlTable += '</table>';
+
+  // create an article element (tag)
+  let article = document.createElement('article');
+
+  // add content tto the article
+  article.innerHTML = htmlTable;
+  // add the article to the main element
+  document.querySelector('main').append(article);
+}
