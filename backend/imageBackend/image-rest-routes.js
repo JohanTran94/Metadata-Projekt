45269bb919e215be7d46a3473afd1de22c7d2b28
thomasavguld <rosen.thomas@gmail.com>
2025-09-09 10:50:48 +0200

    path edit for image routes

--- backend/imageBackend/image-rest-routes.js
+++ backend/imageBackend/image-rest-routes.js
@@ -20,7 +20,7 @@ export default function setupImageRestRoutes(app, db) {
 
   router.get('/api/image/metadata', async (_req, res) => {
 
-    const imagesFolder = path.resolve(process.cwd(), 'frontend/warehouse/image');
+    const imagesFolder = path.resolve(process.cwd(), 'warehouse/image');
 
     try {
       const files = fs.readdirSync(imagesFolder)
